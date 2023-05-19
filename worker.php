439932d07f07145283c583e80496f12c2ad405cb
mohammdAmiri <53963857+mawebcoder@users.noreply.github.com>
2023-05-19 20:33:53 +0330

    Update worker.php

--- worker.php
+++ worker.php
@@ -2,14 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use App\MessagingInterface;
+use App\MessagingService;
 use Illuminate\Contracts\Http\Kernel;
-use Proto\Service\MyServiceInterfaceImpl;
-use Spiral\RoadRunner\Worker;
-use Spiral\Goridge\StreamRelay;
-use Spiral\RoadRunner\GRPC\Server;
 use Illuminate\Http\Request;
-
+use Spiral\Goridge\StreamRelay;
 use Spiral\RoadRunner\GRPC\Invoker;
+use Spiral\RoadRunner\GRPC\Server;
+use Spiral\RoadRunner\Worker;
 
 
 $app = require_once __DIR__ . '/bootstrap/app.php';
@@ -18,15 +18,14 @@ $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
-)->send();
+);
 
 
 $worker = new Worker(new StreamRelay(STDIN, STDOUT));
 
 $server = new Server(new Invoker());
 
-$server->registerService(\Proto\Service\MyServiceInterface::class, new MyServiceInterfaceImpl());
+$server->registerService(MessagingInterface::class, new MessagingService());
 
 $server->serve($worker);
-
 
